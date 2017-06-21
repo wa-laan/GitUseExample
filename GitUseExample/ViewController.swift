7c557b8fa7f5a8a5895d7ec0c258305a81e4e615
Andre Laan <a.laan@vdlz.nl>
2017-06-21 15:51:13 +0200

    finished tutorial

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -16,8 +16,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+        print("You finished!")
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
