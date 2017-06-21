9dc57b0f0f548327e57d40acd1d9c39f2e941af8
Andre Laan <a.laan@vdlz.nl>
2017-06-21 14:44:29 +0200

    added MapForItinerary class to branche 'map_feature'

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,15 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Andre Laan on 21-06-17.
+//  Copyright © 2017 VanderLaan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapForItinerary: NSObject
+{
+    
+}
